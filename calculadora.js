bc2de3c6d5c56961cd0382fd9c4a3d029245891e
Matheus Conde Nobre <matheuscnobre2010@hotmail.com>
2020-10-25 13:01:01 -0300

    Add files via upload
    Programa feito utilizando arrows functions e callbacks aprendidos

---
+++ calculadora.js
@@ -0,0 +1,9 @@
+const somar = (numero1,numero2) => numero1 + numero2;
+const subtrair = (numero1,numero2) => numero1 - numero2;
+const multiplicar = (numero1,numero2) => numero1 * numero2;
+const dividir = (numero1,numero2) => numero1 / numero2;
+//da linha 1 a linha 4 são definidas as operações matemáricas que serão utilizadas, por meio de arrows functions
+const calculadora = (numero1, numero2, operacao) => operacao(numero1,numero2);
+//na linha 6 é feita a função que irá chamar a operação, por meio de um callback
+console.log(calculadora(10,20,multiplicar))
+//o resultado final, mostrado no console.log, é 300, visto que a operação é 20 vezes 10
