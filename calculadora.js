812470fa815d8e9f659264ff612e03877a761c9c
Matheus Conde Nobre <matheuscnobre2010@hotmail.com>
2020-10-25 13:01:40 -0300

    Update calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -2,7 +2,7 @@ const somar = (numero1,numero2) => numero1 + numero2;
 const subtrair = (numero1,numero2) => numero1 - numero2;
 const multiplicar = (numero1,numero2) => numero1 * numero2;
 const dividir = (numero1,numero2) => numero1 / numero2;
-//da linha 1 a linha 4 são definidas as operações matemáricas que serão utilizadas, por meio de arrows functions
+//da linha 1 a linha 4 são definidas as operações matemáticas que serão utilizadas, por meio de arrows functions
 const calculadora = (numero1, numero2, operacao) => operacao(numero1,numero2);
 //na linha 6 é feita a função que irá chamar a operação, por meio de um callback
 console.log(calculadora(10,20,multiplicar))
