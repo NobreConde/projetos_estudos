94cb36add90b18c4b789393f7ed358407abeb281
Matheus Conde Nobre <matheuscnobre2010@hotmail.com>
2020-10-27 09:09:54 -0300

    Update contadorDeMoedas.js

--- contadorDeMoedas.js
+++ contadorDeMoedas.js
@@ -1,12 +1,14 @@
-var a = 49; //a é a moeda de 5 centavos
-var b = 28; //b é a moeda de 10 centavos
-var c = 2; //c é a moeda de 25 centavos
-var d = 0; //d é a moeda de 50 centavos
-var e = 0; //e é a moeda de 1 real
+// var five = 49; five é a moeda de 5 centavos
+// var ten = 28; ten é a moeda de 10 centavos
+// var twenty_five = 2; twenty_five é a moeda de 25 centavos
+// var fifty = 0; fifty é a moeda de 50 centavos
+// var one = 0; one é a moeda de 1 real
 
-function contarMoedas(){
-    let resultado = (a*0.05)+(b*0.10)+(c*0.25)+(d*50)+(e*1)
-    console.log("Você tem um total de R$" + resultado)
+function contarMoedas(five,ten,twenty_five,fifty,one){ //Indicando cada variavel para a conta
+    let resultado = (five*0.05)+(ten*0.10)+(twenty_five*0.25)+(fifty*50)+(one*1) //Contas efetuadas com cada variavel
+    console.log("Você tem um total de R$" + resultado) //Impressão do resultado mais frase
 }
 
-contarMoedas()
+contarMoedas(49,28,2,0,0) //Simulação e resultado logo abaixo
+
+// Você tem um total de R$5.75
